421fc285de47fc576427d6288d27700ed9dd08af
trkfsch475 <t.alfashtaki@andagon.com>
2018-09-26 10:27:36 +0200

    added hello world program

---
+++ Hallo.java
@@ -0,0 +1,13 @@
+
+public class Hallo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i=0; i<=9;i++) {
+	
+	    System.out.println("Hello andagon..."+ i);
+      }
+		
+	}
+
+}
